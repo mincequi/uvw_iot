6235dc7847d2e344ff1e8c149658a7ad30128ba1
Manuel <mincequi@web.de>
2025-10-20 22:46:47 +0200

    Renovate

--- uvw_iot/util/Filter.cpp
+++ uvw_iot/util/Filter.cpp
@@ -1,12 +1,13 @@
 #include "Filter.h"
 
 #include <algorithm>
+#include <cmath>
 
 namespace uvw_iot::util {
 
 void ema(int& prev, int curr, std::chrono::milliseconds deltaT, std::chrono::milliseconds tau) {
     deltaT = std::min(deltaT, tau);
-    prev += (curr - prev) * (double)deltaT.count() / (double)tau.count();
+    prev += round((curr - prev) * (double)deltaT.count() / (double)tau.count());
 }
 
 } // namespace uvw_iot::util
